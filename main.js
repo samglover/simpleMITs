9fa848267a8d71cd4d1c10dacd74eca479e39cc6
samglover <sam@samglover.net>
2020-10-29 16:25:20 -0500

    0.3.2

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function getMITs() {
                               '<div class="col-auto py-3 pl-2 pr-1 dragHandle text-muted">&uarr;<br/>&darr;</div>' +
                               '<div class="col-auto px-2 py-3"><a type="button" class="badge badge-pill badge-secondary p-0 taskNum" href="#" role="checkbox"' + checked + ' onclick="changeStatus( \''+id+'\' )"><span class="number">' + ( i + 1 ) + '</span><span class="checkmark">&check;</span></a></div>' +
                               '<div class="col align-items-center px-2 py-3">' +
-                                '<span class="taskDesc mb-0" id="' + id + '_desc" contenteditable="true">' + desc + '</span> ' +
+                                '<span class="taskDesc mb-0" id="' + id + '_desc">' + desc + '</span> ' +
                               '</div>' +
                               '<div class="col col-auto p-3"><button type="button" class="close text-muted taskDel" onclick="delTask( \''+id+'\' )">&times;</button></div>' +
                             '</div>' +
@@ -86,13 +86,6 @@ function getMITs() {
   // Assigns event listeners for drag-and-drop reordering.
   let tasks = taskList.childNodes;
   [].forEach.call( tasks, addDragHandlers );
-
-  // Assigns event listeners for task editing.
-  let taskDescs = document.getElementsByClassName( '.taskDesc' );
-  [].forEach.call( tasks, function( e ) {
-    e.addEventListener( 'focusin', editTask );
-    e.addEventListener( 'focusout', editTask );
-  });
 
   let tasksNotDone  = $( '.task.notDone' ).length;
   let tasksDone     = $( '.task.done' ).length;
@@ -295,34 +288,6 @@ function saveTask( e ) {
   getMITs();
 
   e.preventDefault();
-
-}
-
-
-function editTask( e ) {
-
-  console.log( e );
-
-  /*
-
-  let editedMIT   = document.getElementById( id );
-  let storedMITs  = JSON.parse( localStorage.getItem( 'simpleMITs' ) );
-
-  console.log( editedMIT );
-
-  for ( let i = 0; i < storedMITs.length; i++ ) {
-
-    if ( storedMITs[ i ].id == id ) {
-      storedMITs[ i ].description = editedMIT.description.trim();
-    }
-
-  }
-
-  localStorage.setItem( 'simpleMITs', JSON.stringify( mits ) );
-
-  getMITs();
-
-  */
 
 }
 
